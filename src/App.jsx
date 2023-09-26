57795b6322f0153e9d7432c062c7d40eb73e8348
Triss Marsh <marshtriss@gmail.com>
2023-09-26 15:42:19 +0100

    vite mdbootstrap setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './style.css'
+
+function App(props) {
+  return (
+    <>
+    <div className="todoapp stack-large">
+      <h1>ListEase</h1>
+      
+    </div>
+    </>
+  );
+}
+
+export default App
