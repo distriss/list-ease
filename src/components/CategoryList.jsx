52788938e77c6b67540b28da17539806b286715e
Triss Marsh <marshtriss@gmail.com>
2023-09-30 21:48:03 +0100

    rollback commit

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { MDBListGroup } from 'mdb-react-ui-kit';
 import CategoryItem from './CategoryItem';
 
-export default function CategoryList({ categories, toggleCategory, deleteCategory, selectedCategory }) {
+export function CategoryList({ categories, toggleCategory, deleteCategory, selectedCategory }) {
   return (
     <MDBListGroup style={{ minWidth: '22rem' }} light>
-      {categories.length === 0 && <MDBListGroupItem>No Categories</MDBListGroupItem>}
+      {categories.length === 0 && "No Categories"}
       {categories.map(category => (
         <CategoryItem
           key={category.id}
